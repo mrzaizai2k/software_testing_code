ca1443e7c1e757f048a497b2b7d5abc588b1ccf9
Mai Chí Bảo <chibao24.12.1999@gmail.com>
2024-05-02 10:51:32 +0700

    Update auto_test.py

--- auto_test.py
+++ auto_test.py
@@ -20,7 +20,6 @@ def initialize_driver(driver_path, browser_options):
 config = read_config(path = "config.yml")
 
 driver_path = config['driver_path']
-# driver_path = "chromedriver-mac-x64/chromedriver-mac-x64/chromedriver"
 
 browser_options = Options()
 browser_options.add_argument("--no-sandbox")
@@ -32,8 +31,6 @@ driver, webdriver_service = initialize_driver(driver_path, browser_options)
 # Open the URL in the Chrome browser
 driver.get("https://www.calculator.net/bmi-calculator.html")
 
-# Check if the login page is opened by searching for the username input field
-# username_input = driver.find_element(By.XPATH, '//*[@id="username"]')
 time.sleep(1)
 
 def check_result_BMI(driver, config):
@@ -53,15 +50,15 @@ def check_result_BMI(driver, config):
         print ('bmi_value', bmi_value)
         print ('status', status)
         if bmi_value == str(config['bmi_value']) and status == config['status']:
-            print ('Test case: Passed')
+            return True 
         else:
-            print ('Test case: Not Passed')
+            return False 
+
 
     except Exception as e:
         print("Not appear result element:")
+        return False 
 
-    # Step 3: Return the extracted data
-    return bmi_value, status
 
 def check_age_error(driver, config):
     # Validate age
@@ -95,20 +92,7 @@ def check_height_error(driver, config, error = "Please provide positive height v
         return False
 
 
-# def check_weight_error(driver, config, error = "Please provide positive height value."):
-
-#     # Check for error message
-#     time.sleep(1)
-#     try:
-#         error_message = driver.find_element(By.XPATH, "//font[@color='red']")
-#         if error in error_message.text:
-#             return True
-#         else:
-#             return False
-#     except:
-#         return False
-
-def check_weight_error(driver, config, error="Please provide positive height value."):
+def check_weight_error(driver, config, error="Please provide positive weight value."):
     # Wait for a short period to ensure all elements are loaded
     time.sleep(1)
     
@@ -151,74 +135,101 @@ def check_waist_error(driver, config, error = "Waist need to be numeric."):
 
 
 def input_values_BMI(driver, config):
-    metric_link = driver.find_element(By.XPATH, '//a[contains(text(), "Metric Units")]')
-    metric_link.click()
-
-    age_input = driver.find_element(By.ID, 'cage')
-    age_input.clear()
-    if config['age'] is not None:
-        age_input.send_keys(config['age'])
-
-    height_input = driver.find_element(By.ID, 'cheightmeter')
-    height_input.clear()
-    if config['height'] is not None:
-        height_input.send_keys(config['height'])
-
-
-    weight_input = driver.find_element(By.ID, 'ckg')
-    weight_input.clear() 
-    if config['weight'] is not None:
-        weight_input.send_keys(config['weight'])
-
-
     
-    # Select gender based on config
-    time.sleep(2)
-        # Select gender based on config
-    if config['gender'] == 'male':
-        radio = driver.find_element(By.XPATH,"//label[contains(text(),'Male')]")
-        radio.click()
-    elif config['gender'] == 'female':
-        radio = driver.find_element(By.XPATH,"//label[contains(text(),'Female')]")
-        radio.click()
-    else:
-        print("Invalid gender specified in config")
+    max_retries = 3
+    attempt = 0
+
+    while attempt < max_retries:
+        try:
+            print(f'config: {config}')
+            age_flag =True
+            height_flag =True 
+            weight_flag=True
+            result_BMI =True 
+                                                                                
+            metric_link = driver.find_element(By.XPATH, '//a[contains(text(), "Metric Units")]')
+            metric_link.click()
+
+            age_input = driver.find_element(By.ID, 'cage')
+            age_input.clear()
+            if config['age'] is not None:
+                age_input.send_keys(config['age'])
+
+            height_input = driver.find_element(By.ID, 'cheightmeter')
+            height_input.clear()
+            if config['height'] is not None:
+                height_input.send_keys(config['height'])
 
 
-    # Click on the "Calculate" button
-    calculate_button = driver.find_element(By.XPATH, '//input[@value="Calculate"]')
-    calculate_button.click()
-
-    if config['age'] < 2 or config['age'] > 120:
-        age_flag = check_age_error(driver, config)
-        print("age_flag", age_flag)
-
-    if config['height'] <= 0:
-        height_flag = check_height_error(driver, config)
-        print("height_flag", height_flag)
-
-    if config['weight'] <= 0:
-        weight_flag = check_weight_error(driver, config)
-        print("weight_flag", weight_flag)
+            weight_input = driver.find_element(By.ID, 'ckg')
+            weight_input.clear() 
+            if config['weight'] is not None:
+                weight_input.send_keys(config['weight'])
 
 
-    check_result_BMI(driver, config)
+            
+            # Select gender based on config
+            time.sleep(2)
+                # Select gender based on config
+            if config['gender'] == 'male':
+                radio = driver.find_element(By.XPATH,"//label[contains(text(),'Male')]")
+                radio.click()
+            elif config['gender'] == 'female':
+                radio = driver.find_element(By.XPATH,"//label[contains(text(),'Female')]")
+                radio.click()
+            else:
+                print("Invalid gender specified in config")
+
+
+            # Click on the "Calculate" button
+            calculate_button = driver.find_element(By.XPATH, '//input[@value="Calculate"]')
+            calculate_button.click()
+
+            if config['age'] < 2 or config['age'] > 120:
+                age_flag = check_age_error(driver, config)
+                print("age_flag", age_flag)
+
+            if config['height'] <= 0:
+                height_flag = check_height_error(driver, config)
+                print("height_flag", height_flag)
+
+            if config['weight'] <= 0:
+                weight_flag = check_weight_error(driver, config)
+                print("weight_flag", weight_flag)
+
+            if config['bmi_value'] is not None:
+                result_BMI = check_result_BMI(driver, config)
+
+            if (age_flag==False) or (height_flag==False) or (weight_flag==False) or (result_BMI==False):
+                print(f'Test case: Failed!')
+            else:
+                print(f'Test case: Passed!')
+            return  
+
+        except Exception as e:
+                print(f"Attempt {attempt + 1} failed with error: {e}")
+                attempt += 1
+                if attempt < max_retries:
+                    print("Retrying after 5 seconds...")
+                    time.sleep(5)
+        
 
 
 for test_case in config['BMI_test']:
     try:
+        print(f'*********************')
         print(f'\nTest for case: {test_case}')
         input_values_BMI(driver, config['BMI_test'][test_case])
         time.sleep(2)
     except Exception as e:
         print(f"error: {e}")
     
+
 def check_result_body_fat(driver, config):
     body_fat_value = None
-    status = None
     try:
         # Adjust the timeout as needed; here it's set to 10 seconds
-        result_element = WebDriverWait(driver, 3).until(
+        result_element = WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "p.verybigtext font, font"))
         )
         result_text = result_element.get_attribute('innerHTML')
@@ -228,22 +239,18 @@ def check_result_body_fat(driver, config):
 
         print ('body_fat', body_fat_value)
         if str(body_fat_value) == str(config['body_fat']):
-            print ('Test case: Passed')
+            return True
         else:
-            print ('Test case: Not Passed')
+            return False
 
     except Exception as e:
-        print("Not appear result element:")
-        print(f"Error result body fat: {e}")
-
-    # Step 3: Return the extracted data
-    return body_fat_value, status
+        return False
 
 
 def check_age_error_body(driver, config):
     # Validate age
     age = config['age']  # Default to 0 if 'age' key is missing
-    if age<0 or age is None:
+    if (config['age'] is not None and config['age'] < 0) or config['age'] is None:
         # Check for error message
         time.sleep(1)  # Wait for the error message to potentially load
         try:
@@ -258,84 +265,112 @@ def check_age_error_body(driver, config):
         pass
 
 def input_values_body_fat(driver, config):
-    print("config", config)
-    metric_link = driver.find_element(By.XPATH, '//a[contains(text(), "Metric Units")]')
-    metric_link.click()
+    max_retries = 3
+    attempt = 0
 
-    age_input = driver.find_element(By.NAME, 'cage')
-    age_input.clear()
-    print("config['neck']", config['neck'])
-    if config['age'] is not None:
-        age_input.send_keys(config['age'])
+    while attempt < max_retries:
+        try:
+            print("config", config)
+            age_flag =True
+            height_flag =True 
+            weight_flag=True
+            body_fat_result =True 
+            neck_flag=True
+            waist_flag=True
 
-    height_input = driver.find_element(By.NAME, 'cheightmeter')
-    height_input.clear()
-    if config['height'] is not None:
-        height_input.send_keys(config['height'])
+            metric_link = driver.find_element(By.XPATH, '//a[contains(text(), "Metric Units")]')
+            metric_link.click()
 
+            age_input = driver.find_element(By.NAME, 'cage')
+            age_input.clear()
 
-    weight_input = driver.find_element(By.NAME, 'cweightkgs')
-    weight_input.clear() 
-    if config['weight'] is not None:
-        weight_input.send_keys(config['weight'])
+            if config['age'] is not None:
+                age_input.send_keys(config['age'])
 
-    neck_input = driver.find_element(By.NAME, 'cneckmeter')
-    neck_input.clear()
-    if config['neck'] is not None:
-        neck_input.send_keys(config['neck'])
-
-    waist_input = driver.find_element(By.NAME, 'cwaistmeter')
-    waist_input.clear() 
-    if config['waist'] is not None:
-        waist_input.send_keys(config['waist'])
-
-    
-    # Select gender based on config
-    time.sleep(2)
-        # Select gender based on config
-    if config['gender'] == 'male':
-        radio = driver.find_element(By.XPATH,"//label[contains(text(),'male')]")
-        radio.click()
-    elif config['gender'] == 'female':
-        radio = driver.find_element(By.XPATH,"//label[contains(text(),'female')]")
-        radio.click()
-    else:
-        print("Invalid gender specified in config")
+            height_input = driver.find_element(By.NAME, 'cheightmeter')
+            height_input.clear()
+            if config['height'] is not None:
+                height_input.send_keys(config['height'])
 
 
-    # Click on the "Calculate" button
-    calculate_button = driver.find_element(By.XPATH, '//input[@value="Calculate"]')
-    calculate_button.click()
+            weight_input = driver.find_element(By.NAME, 'cweightkgs')
+            weight_input.clear() 
+            if config['weight'] is not None:
+                weight_input.send_keys(config['weight'])
+
+            neck_input = driver.find_element(By.NAME, 'cneckmeter')
+            neck_input.clear()
+            if config['neck'] is not None:
+                neck_input.send_keys(config['neck'])
+
+            waist_input = driver.find_element(By.NAME, 'cwaistmeter')
+            waist_input.clear() 
+            if config['waist'] is not None:
+                waist_input.send_keys(config['waist'])
+
+            
+            # Select gender based on config
+            time.sleep(2)
+                # Select gender based on config
+            if config['gender'] == 'male':
+                radio = driver.find_element(By.XPATH,"//label[contains(text(),'male')]")
+                radio.click()
+            elif config['gender'] == 'female':
+                radio = driver.find_element(By.XPATH,"//label[contains(text(),'female')]")
+                radio.click()
+            else:
+                print("Invalid gender specified in config")
 
 
-    if (config['age'] is not None and config['age'] < 0) or config['age'] is None:
-        age_flag = check_age_error_body(driver, config)
-        print("age_flag", age_flag)
+            # Click on the "Calculate" button
+            calculate_button = driver.find_element(By.XPATH, '//input[@value="Calculate"]')
+            calculate_button.click()
 
-    if (config['height'] is not None and config['height'] < 0) or config['height'] is None:
-        height_flag = check_height_error(driver, config, error = "Height need to be positive.")
-        print("height_flag", height_flag)
+            if (config['age'] is not None and config['age'] < 0) or config['age'] is None:
+                age_flag = check_age_error_body(driver, config)
+                print("age_flag", age_flag)
 
-    if (config['weight'] is not None and config['weight'] < 0) or config['weight'] is None:
-        weight_flag = check_weight_error(driver, config)
-        print("weight_flag", weight_flag)
+            if (config['height'] is not None and config['height'] < 0) or config['height'] is None:
+                height_flag = check_height_error(driver, config, error = "Height need to be positive.")
+                print("height_flag", height_flag)
 
-    if (config['neck'] is not None and config['neck'] < 0) or config['neck'] is None:
-        neck_flag = check_weight_error(driver, config, error = "Neck need to be numeric.")
-        print("neck_flag", neck_flag)
+            if (config['weight'] is not None and config['weight'] < 0) or config['weight'] is None:
+                weight_flag = check_weight_error(driver, config, error = "Please provide a positive weight.")
+                print("weight_flag", weight_flag)
 
-    if (config['waist'] is not None and config['waist'] < 0) or config['waist'] is None:
-        waist_flag = check_waist_error(driver, config, error = "Waist need to be numeric.")
-        print("waist_flag", waist_flag)
+            if (config['neck'] is not None and config['neck'] < 0) or config['neck'] is None:
+                neck_flag = check_weight_error(driver, config, error = "Neck need to be numeric.")
+                print("neck_flag", neck_flag)
 
+            if (config['waist'] is not None and config['waist'] < 0) or config['waist'] is None:
+                waist_flag = check_waist_error(driver, config, error = "Waist need to be numeric.")
+                print("waist_flag", waist_flag)
 
-    check_result_body_fat(driver, config)
+            if config['body_fat'] is not None:
+                body_fat_result = check_result_body_fat(driver, config)
+            
+
+            if (age_flag==False) or (height_flag==False) or (weight_flag==False) or (body_fat_result==False)  or (neck_flag==False) or (waist_flag==False) :
+                print(f'Test case: Failed!')
+            else:
+                print(f'Test case: Passed!')
+            
+            return  
+
+        except Exception as e:
+            print(f"Attempt {attempt + 1} failed with error: {e}")
+            attempt += 1
+            if attempt < max_retries:
+                print("Retrying after 5 seconds...")
+                time.sleep(5)
+
 
 driver.get("https://www.calculator.net/body-fat-calculator.html")
 
 time.sleep(1)
 for test_case in config['Body_fat_test']:
     try:
+        print(f'*********************')
         print(f'\nTest for case: {test_case}')
         input_values_body_fat(driver, config['Body_fat_test'][test_case])
         time.sleep(2)
